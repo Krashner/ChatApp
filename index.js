ba7af23c4a8f9418cf36c8aa16f0ce8abab171ba
Krashner <jbuttrey@oddrealms.com>
2019-02-19 13:40:14 -0600

    started seperating files

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-var roles =[];
+var path = require('path');
+var roles = [];
 
 //get the roles from admin file
-fs.readFile('admin.json', 'utf8', function(err, contents) {
-    roles = JSON.parse(contents).roles;
+fs.readFile('admin.json', 'utf8', function (err, contents) {
+  roles = JSON.parse(contents).roles;
 });
 
-app.get('/', function(req, res){
+//server static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    //read the admin file on connect
-    roles.forEach(function(entry) {
-        socket.emit('update role', entry);
-    });
+io.on('connection', function (socket) {
+  //read the admin file on connect
+  roles.forEach(function (entry) {
+    socket.emit('update role', entry);
+  });
 
-    //send the message out
-    socket.on('chat message', function(role, msg){
-        io.emit('chat message', role + " " + timeNow(), msg);
-    });
+  //send the message out
+  socket.on('chat message', function (role, msg) {
+    socket.broadcast.emit('chat message', role, msg);
+  });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
-
-
-function timeNow() {
-    var d = new Date(),
-      h = (d.getHours()<10?'0':'') + d.getHours(),
-      m = (d.getMinutes()<10?'0':'') + d.getMinutes();
-    return h + ':' + m + ': ';
-}
---
+++ public/js/script.js
@@ -0,0 +1,39 @@
+$(function () {
+  var socket = io();
+  var date = new Date();
+  $('form').submit(function (e) {
+    e.preventDefault(); // prevents page reloading
+    //don't allow blank messages
+    var text = $('#m').val();
+    var role = $('#role').val();
+    //send message, clear message box and add message to local chat
+    if (text.replace(/\s+/g, '') !== '' && role !== '') {
+      role += " " + timeNow();
+      socket.emit('chat message', role, text);
+      $('#m').val('');
+      addMessageToLog(role, text);
+    }
+    return false;
+  });
+  //update chat log with recieved message
+  socket.on('chat message', function (header, msg) {
+    addMessageToLog(header, msg);
+  });
+  //get roles
+  socket.on('update role', function (role) {
+    $('#role').append($('<option>').val(role).text(role));
+    console.log(role);
+  });
+  //add messages to log
+  function addMessageToLog(header, msg) {
+    $('#messages').append($('<li class="header">').text(header));
+    $('#messages').append($('<li>').text(msg));
+  }
+  //get a timestamp          
+  function timeNow() {
+    var d = new Date(),
+      h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+      m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+    return h + ':' + m + ': ';
+  }
+});
