de5b1390d9a9e3f58276fa5df3ea32c60149942c
Justin-Pi <jbuttrey@oddrealms.com>
2019-03-09 17:35:58 -0600

    Cleaned up and formatted code

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 //simple peer
 app.use('/simple-peer', express.static(__dirname + '/node_modules/simple-peer'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 //get the roles from admin file
-fs.readFile('admin.json', 'utf8', function (err, contents) {
+fs.readFile('admin.json', 'utf8', function(err, contents) {
     roles = JSON.parse(contents).roles;
 });
 
 //on socket connection
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
 
     //add socket to array
     connectedSockets.push(socket);
@@ -53,7 +53,7 @@ io.on('connection', function (socket) {
     //});
 
     //broadcast the signal to specific socket
-    socket.on('peer call', function (data) {
+    socket.on('peer call', function(data) {
         var d = JSON.parse(data);
         io.to(d.sendSignalTo).emit('peer response', data);
     });
@@ -63,12 +63,12 @@ io.on('connection', function (socket) {
     socket.emit('update roles', roles);
 
     //send the message out
-    socket.on('chat message', function (role, msg) {
+    socket.on('chat message', function(role, msg) {
         socket.broadcast.emit('chat message', role, msg);
     });
 
     //socket has joined channel, temporarily not used
-    socket.on('join', function (role) {
+    socket.on('join', function(role) {
         onlineUsers.push({
             id: socket.id,
             username: role
@@ -79,7 +79,7 @@ io.on('connection', function (socket) {
     });
 
     //change current user role
-    socket.on('role change', function (role) {
+    socket.on('role change', function(role) {
         removeFromUsers(socket.id, role);
         //emit user list to all users
         io.emit('onlineUsers', onlineUsers, Object.keys(onlineUsers).length);
@@ -91,7 +91,7 @@ io.on('connection', function (socket) {
     });
 
     // // To listen for a client's disconnection from server and intimate other clients about the same
-    socket.on('disconnect', function (data) {
+    socket.on('disconnect', function(data) {
         removeFromUsers(socket.id, null);
         connectedSockets = socketRemove(connectedSockets, socket);
         console.log("Connected Sockets" + connectedSockets);
@@ -101,7 +101,7 @@ io.on('connection', function (socket) {
 });
 
 function socketRemove(arr, value) {
-    return arr.filter(function (ele) {
+    return arr.filter(function(ele) {
         return ele != value;
     });
 }
@@ -128,6 +128,6 @@ function findIndex(array, id, role) {
     });
 }
 
-http.listen(3000, function () {
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,264 +1,212 @@
-$(function () {
-  var socket = io();
-  var previousRole;
-  var peers = [];
+$(function() {
+    var socket = io();
+    var previousRole;
+    var peers = [];
 
-  //join the room after a role is selected
-  $("#roles").on('focus', function () {
-    // Store the current value on focus and on change
-    previousRole = this.value;
-  }).change(function () {
-    var role = this.value;
-    if (this.value === 'None') {
-      socket.emit('role change', null);
-    } else {
-      if (previousRole != role) {
-        socket.emit('role change', role);
-        if (role !== null) {
-          socket.emit('join', role);
-          console.log("joining");
-          previous = this.value;
+    //join the room after a role is selected
+    $("#roles").on('focus', function() {
+        // Store the current value on focus and on change
+        previousRole = this.value;
+    }).change(function() {
+        var role = this.value;
+        if (this.value === 'None') {
+            socket.emit('role change', null);
+        } else {
+            if (previousRole != role) {
+                socket.emit('role change', role);
+                if (role !== null) {
+                    socket.emit('join', role);
+                    console.log("joining");
+                    previous = this.value;
+                }
+            }
         }
-      }
+    });
+
+
+    $(".chat-target-btn").click(function() {
+        $(".chat-target-btn").removeClass("active");
+        $(this).addClass("active");
+    });
+
+
+    //create a new peer connection
+    socket.on('add peer', function(isInitiator, targetSocketID) {
+        console.log("create peer");
+        //this method will not work when trying to connect to more than one at a time, needs a fix
+
+        var p = new SimplePeer({
+            initiator: isInitiator,
+            trickle: false,
+            //config: {"iceServers":[]}
+        });
+
+        p.sendSignalTo = targetSocketID;
+        p.signalOriginator = socket.id;
+        p.sendingPeerID = p._id;
+        setPeerListeners(p);
+        peers.push(p);
+    });
+
+    //remove closed peer connection
+    function removePeer() {
+
     }
-  });
-  
-    
-    $(".chat-target-btn").click(function () {
-	$(".chat-target-btn").removeClass("active");
-	$(this).addClass("active");
-    });
-    
 
-  //~ var sendSignalTo;
-  //~ var signalOriginator;
-  //~ var sendingPeerID;
+    function setPeerListeners(peer) {
 
-  //create a new peer connection
-  socket.on('add peer', function (isInitiator, targetSocketID) {
-    console.log("create peer");
-    //this method will not work when trying to connect to more than one at a time, needs a fix
+        //send signal to reciever
+        peer.on('signal', function(data) {
+            data.sendSignalTo = peer.sendSignalTo;
+            //~ signalOriginator = socket.id;
+            data.signalOriginator = peer.signalOriginator;
+            //~ sendingPeerID = p._id;
+            data.sendingPeerID = peer.sendingPeerID;
+            console.log('SIGNAL', JSON.stringify(data));
+            socket.emit('peer call', JSON.stringify(data));
+        });
 
-    var p = new SimplePeer({
-      initiator: isInitiator,
-      trickle: false,
-      //config: {"iceServers":[]}
-    });
-    
-    //~ sendSignalTo = targetSocketID;
-	p.sendSignalTo = targetSocketID;
-    //~ signalOriginator = socket.id;
-  p.signalOriginator = socket.id;
-    //~ sendingPeerID = p._id;
-	p.sendingPeerID = p._id;
-    setPeerListeners(p);
+        //peer connected
+        peer.on('connect', function() {
+            console.log('CONNECT')
+            navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: true
+                })
+                .then(function(stream) {
+                    var video = document.querySelector('#localVideo');
+                    // Older browsers may not have srcObject
+                    if ("srcObject" in video) {
+                        video.srcObject = stream;
+                        peer.addStream(stream);
+                        // video[0].load();
+                        // video[0].play();
+                    } else {
+                        // Avoid using this in new browsers, as it is going away.
+                        //video.src = window.URL.createObjectURL(stream);
+                    }
+                })
+                .catch(function(err) {
+                    console.log(err.name + ": " + err.message);
+                });
+        });
 
-    //var peer = createPeer(isInitiator);
-    peers.push(p);
-    //console.log(peers);
-  });
-
-  //remove closed peer connection
-  function removePeer() {
-
-  }
-
-  function setPeerListeners(peer) {
-
-    //send signal to reciever
-    peer.on('signal', function (data) {
-    	data.sendSignalTo = peer.sendSignalTo;
-      //~ signalOriginator = socket.id;
-      data.signalOriginator = peer.signalOriginator;
-      //~ sendingPeerID = p._id;
-      data.sendingPeerID =	peer.sendingPeerID;
-      console.log('SIGNAL', JSON.stringify(data));
-      //~ console.log("Signal Sent:   Originator: " + signalOriginator + " Target " + sendSignalTo + " PeerID: " + sendingPeerID);
-      socket.emit('peer call', JSON.stringify(data));
-      //socket.emit('peer signal', JSON.stringify(peer), data.type, JSON.stringify(data));
-    });
-
-    //peer connected
-    peer.on('connect', function () {
-      console.log('CONNECT')
-      navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true
-      })
-        .then(function (stream) {
-          var video = document.querySelector('#localVideo');
-          // Older browsers may not have srcObject
-          if ("srcObject" in video) {
-            video.srcObject = stream;
-            peer.addStream(stream);
-            // video[0].load();
-            // video[0].play();
-          } else {
-            // Avoid using this in new browsers, as it is going away.
-            //video.src = window.URL.createObjectURL(stream);
-          }
+        //data channel is being used
+        peer.on('data', function(data) {
+            console.log('data: ' + data);
         })
-        .catch(function (err) {
-          console.log(err.name + ": " + err.message);
+
+        //streaming
+        peer.on('stream', function(stream) {
+            console.log("STREAM");
+            var video = document.querySelector('#remoteVideo');
+            // Older browsers may not have srcObject
+            video.srcObject = stream;
+        })
+
+        //close connection
+        peer.on('close', function() {
+            console.log("CLOSE");
+        })
+
+        //error
+        peer.on('error', function(err) {
+            console.log('error', err)
+        })
+
+    }
+
+    //peer response to signal
+    socket.on('peer response', function(data) {
+        var d = JSON.parse(data);
+        console.log(d);
+        console.log(peers);
+        //if an offer is recieved, create a non-iniator peer and respond
+        if (d.type === "offer") {
+            console.log("create peer 2");
+            //create a non-initiating peer
+            var p = new SimplePeer({
+                initiator: false,
+                trickle: false,
+                //config: {"iceServers":[]}
+            });
+            p.sendSignalTo = d.signalOriginator;
+            p.signalOriginator = d.sendSignalTo;
+            p.sendingPeerID = d.sendingPeerID;
+            console.log("Offer Recieved:   Originator: " + p.signalOriginator + " Target " + p.sendSignalTo + " PeerID: " + d.sendingPeerID);
+            setPeerListeners(p);
+            p.signal(data);
+            peers.push(p);
+        } else {
+            console.log("Answer Recieved:   Originator: " + d.signalOriginator + " Target " + d.sendSignalTo + " PeerID: " + d.sendingPeerID);
+            //if an answer is recieved, give data to original peer
+            console.log("got an answer");
+
+            for (var i = 0; i < peers.length; i++) {
+                if (peers[i]._id === d.sendingPeerID) {
+                    console.log("connecting call");
+                    peers[i].signal(data);
+                }
+            }
+        }
+    });
+
+
+    $('form').submit(function(e) {
+        e.preventDefault(); // prevents page reloading
+        //don't allow blank messages
+        var text = $('#chat-input').val();
+        var role = $('#roles').val();
+        //send message, clear message box and add message to local chat
+        if (text.replace(/\s+/g, '') !== '' && role !== '') {
+            role += " " + timeNow();
+            socket.emit('chat message', role, text);
+            //peer.send(text);
+            $('#chat-input').val('');
+            addMessageToLog(role, text);
+            $('#chat-input').focus();
+        }
+        return false;
+    });
+
+
+    //update chat log with recieved message
+    socket.on('chat message', function(header, msg) {
+        addMessageToLog(header, msg);
+    });
+
+    //get the user roles
+    socket.on('update roles', function(roles) {
+        $('#roles').empty();
+        $('#roles').append($('<option>').val("None").text("None"));
+        roles.forEach(function(entry) {
+            $('#roles').append($('<option>').val(entry).text(entry));
         });
     });
 
-    //data channel is being used
-    peer.on('data', function (data) {
-      console.log('data: ' + data);
-    })
-
-    //streaming
-    peer.on('stream', function (stream) {
-      console.log("STREAM");
-      var video = document.querySelector('#remoteVideo');
-      // Older browsers may not have srcObject
-      video.srcObject = stream;
-    })
-
-    //close connection
-    peer.on('close', function () {
-      console.log("CLOSE");
-    })
-
-    //error
-    peer.on('error', function (err) {
-      console.log('error', err)
-    })
-
-  }
-
-  //peer response to signal
-  socket.on('peer response', function (data) {
-    var d = JSON.parse(data);
-    //~ var signalOriginator = initiatorID;
-    //~ var sendingPeerID = peerID;
-    console.log(d);
-    console.log(peers);
-    //if an offer is recieved, create a non-iniator peer and respond
-    if (d.type === "offer") {
-      //~ sendSignalTo = initiatorID;
-      //~ signalOriginator = targetID;
-       
-      console.log("create peer 2");
-      //create a non-initiating peer
-      var p = new SimplePeer({
-        initiator: false,
-        trickle: false,
-        //config: {"iceServers":[]}
-      });
-      p.sendSignalTo = d.signalOriginator;
-      p.signalOriginator = d.sendSignalTo;
-      p.sendingPeerID = d.sendingPeerID;
-      console.log("Offer Recieved:   Originator: " + p.signalOriginator + " Target " + p.sendSignalTo + " PeerID: " + d.sendingPeerID);
-      setPeerListeners(p);
-      p.signal(data);
-      peers.push(p);
-    } else {
-      //~ sendSignalTo = targetID;
-      //~ signalOriginator = socket.id;
-      //~ d.sendSignalTo = p.sendSignalTo;
-      //~ d.signalOriginator = p.signalOriginator;
-      console.log("Answer Recieved:   Originator: " + d.signalOriginator + " Target " + d.sendSignalTo + " PeerID: " + d.sendingPeerID);
-      //if an answer is recieved, give data to original peer
-      console.log("got an answer");
-      //peers[0].signal(data);
-
-      for (var i = 0; i < peers.length; i++) {
-        if (peers[i]._id === d.sendingPeerID) {
-          console.log("connecting call");
-          peers[i].signal(data);
-        }
-      }
-    }
-  });
-
-  //peer answered call
-  socket.on('peer answer', function (peer, type, data) {
-    console.log("RECIEVED ", data);
-
-    //if(type==="offer"){
-    //console.log("offer");
-    //var peer = createPeer(false);
-    //peer.signal(data);
-    ////peers.push({});
-    //}else if(type==="answer"){
-    //var originalPeer = JSON.parse(peer);
-    //console.log(originalPeer._id);
-    //peers.forEach(element => {
-    //if(element.Peer._id === originalPeer._id){
-    //element.signal(data);
-    //return;
-    //}
-    //});
-    //}
-
-  });
-
-  function createPeer(isInitiator) {
-    //var p = new SimplePeer({
-    //  initiator: isInitiator,
-    //   trickle: false
-    //});
-    //setPeerListeners(p);
-    //return p;
-  }
-
-  $('form').submit(function (e) {
-    e.preventDefault(); // prevents page reloading
-    //don't allow blank messages
-    var text = $('#chat-input').val();
-    var role = $('#roles').val();
-    //send message, clear message box and add message to local chat
-    if (text.replace(/\s+/g, '') !== '' && role !== '') {
-      role += " " + timeNow();
-      socket.emit('chat message', role, text);
-      //peer.send(text);
-      $('#chat-input').val('');
-      addMessageToLog(role, text);
-      $('#chat-input').focus();
-    }
-    return false;
-  });
-
-
-  //update chat log with recieved message
-  socket.on('chat message', function (header, msg) {
-    addMessageToLog(header, msg);
-  });
-
-  //get the user roles
-  socket.on('update roles', function (roles) {
-    $('#roles').empty();
-    $('#roles').append($('<option>').val("None").text("None"));
-    roles.forEach(function (entry) {
-      $('#roles').append($('<option>').val(entry).text(entry));
+    //track users online
+    socket.on('onlineUsers', function(onlineUsers, count) {
+        console.log(onlineUsers);
+        $('#clients').empty();
+        $('#clients').append($('<option>').val("None").text("None"));
+        onlineUsers.forEach(function(entry) {
+            if (entry.id !== socket.id && entry.username !== "")
+                $('#clients').append($('<option>').val(entry).text(entry.username + " : " + entry.id));
+        });
     });
-  });
 
-  //track users online
-  socket.on('onlineUsers', function (onlineUsers, count) {
-    console.log(onlineUsers);
-    $('#clients').empty();
-    $('#clients').append($('<option>').val("None").text("None"));
-    onlineUsers.forEach(function (entry) {
-      if (entry.id !== socket.id && entry.username !== "")
-        $('#clients').append($('<option>').val(entry).text(entry.username + " : " + entry.id));
-    });
-  });
+    //add messages to log
+    function addMessageToLog(header, msg) {
+        $('#messages').append($('<li class="header">').text(header));
+        $('#messages').append($('<li>').text(msg));
+    }
 
-  //add messages to log
-  function addMessageToLog(header, msg) {
-    $('#messages').append($('<li class="header">').text(header));
-    $('#messages').append($('<li>').text(msg));
-  }
-
-  //get a timestamp
-  function timeNow() {
-    var d = new Date(),
-      h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
-      m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
-    return h + ':' + m + ': ';
-  }
+    //get a timestamp
+    function timeNow() {
+        var d = new Date(),
+            h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+            m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+        return h + ':' + m + ': ';
+    }
 
 });
