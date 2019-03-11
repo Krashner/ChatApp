e6725fb631701a007e63eb63dd394682c98b9e9d
Justin-Pi <jbuttrey@oddrealms.com>
2019-03-10 19:09:53 -0500

    Significant clean up on server code

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 var path = require('path');
 
 var roles = [];
-var onlineUsers = [];
 var connectedSockets = [];
 
 
@@ -49,7 +48,6 @@ io.on('connection', function(socket) {
         io.to(d.sendSignalTo).emit('peer response', data);
     });
 
-    console.log("Connected Sockets:" + connectedSockets);
     //send roles to clients
     socket.emit('update roles', roles);
 
@@ -60,30 +58,16 @@ io.on('connection', function(socket) {
 
     //socket has joined channel, temporarily not used
     socket.on('join', function(role) {
-        onlineUsers.push({
-            id: socket.id,
-            username: role
-        });
-        console.log(onlineUsers);
-        console.log("-----------------------------------------------------");
-        io.emit('onlineUsers', onlineUsers, Object.keys(onlineUsers).length);
+
     });
 
     //change current user role
     socket.on('role change', function(role) {
-        removeFromUsers(socket.id, role);
-        //emit user list to all users
-        io.emit('onlineUsers', onlineUsers, Object.keys(onlineUsers).length);
-        console.log(onlineUsers.indexOf({
-            id: socket.id,
-            username: role
-        }));
-        findIndex(onlineUsers, socket.id, role);
+
     });
 
     // // To listen for a client's disconnection from server and intimate other clients about the same
     socket.on('disconnect', function(data) {
-        removeFromUsers(socket.id, null);
         connectedSockets = socketRemove(connectedSockets, socket);
         console.log("Connected Sockets" + connectedSockets);
         //remove peer for this socket from every client
@@ -94,28 +78,6 @@ io.on('connection', function(socket) {
 function socketRemove(arr, value) {
     return arr.filter(function(ele) {
         return ele != value;
-    });
-}
-
-function removeFromUsers(id, safeRole) {
-    var result = onlineUsers.filter(obj => {
-        return obj.id === id && safeRole !== obj.role
-    })
-
-    result.forEach(element => {
-        var index = onlineUsers.indexOf(element);
-        onlineUsers.splice(index, 1);
-    });
-}
-
-function findIndex(array, id, role) {
-    var result = array.filter(obj => {
-        return obj.id === id && obj.role !== role
-    })
-
-    result.forEach(element => {
-        var index = onlineUsers.indexOf(element);
-        console.log(index);
     });
 }
 
