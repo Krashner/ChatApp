73eb07f3883f53c22568eb0ab77af4a1d1f290e0
Krashner <jbuttrey@oddrealms.com>
2020-03-08 22:05:34 -0500

    debug

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ function createDB() {
     pgtools.createdb(config, "ChatAppDB", function (err, res) {
         if (err) {
             console.log('> Database Found');
+            console.log(err);
         } else {
             console.log('> Creating Database');
             console.log('> Creating Table');
